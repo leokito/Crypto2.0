2345ce399093453182eb259d48a5092f95180cba
leokito <leonardo.psico@outlook.com.br>
2022-07-13 16:11:33 -0300

    landing page and project upgrade

---
+++ assets/script/landingPage.js
@@ -0,0 +1,26 @@
+const heroContainer = document.createElement("div")
+    heroContainer.classList.add("heroContainer")
+    document.body.appendChild(heroContainer)
+
+const backGroundVid = document.createElement("video")
+    backGroundVid.classList.add("bgVid")
+    backGroundVid.src = "./assets/video/bgvid.mp4"
+    backGroundVid.autoplay = true
+    backGroundVid.loop = true
+    heroContainer.appendChild(backGroundVid)
+
+const titulo = document.createElement("h1")
+    titulo.innerText = "Bem-vindo ao Crypto 2.0"
+    titulo.classList.add("titulo")
+    heroContainer.appendChild(titulo)
+
+const subtitle = document.createElement("p")
+    subtitle.innerText = "Investimentos cripto de maneira rápida, segura e sem complicações."
+    subtitle.classList.add("subtitulo")
+    heroContainer.appendChild(subtitle)
+
+const link = document.createElement("a")
+    link.innerText = "Saiba Mais"
+    link.classList.add("link")
+    link.href = "/homePage.html"
+    heroContainer.appendChild(link)
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,65 @@
+let cryptoMoedas = [
+  {
+    nome: "Bitcoin",
+    valor: 1000,
+    descricao: "A criptomoeda surgiu em 2009, criada pelo Satoshi Nakamoto, como uma forma de facilitar as negociações pela internet."
+
+  },
+  {
+    nome: "Ethereum",
+    valor: 500,
+    descricao: "O foco do Ethereum é ser uma plataforma de programação para aplicações descentralizadas e contratos inteligentes"
+
+  },
+  {
+    nome: "Tether",
+    valor: 100,
+    descricao: "é uma criptomoeda com um preço fixo medido por uma moeda fiduciária, neste caso, os dólares americanos."
+  },
+]
+
+const container = document.querySelector(".cardContainer")
+
+const criarCard = (crypto) => {
+
+  let nome = crypto.nome
+  let preco = crypto.valor
+  let descricao = crypto.descricao
+
+  const card = document.createElement("div")
+  const cartTitle = document.createElement("h3")
+  const cardPrice = document.createElement('p')
+  const cardDescription = document.createElement("p")
+  const cardButton = document.createElement("button")
+  const cardLine = document.createElement("div")
+
+   cardLine.classList.add("line")
+   cardPrice.classList.add("price")
+
+  cartTitle.innerText = nome
+  cardPrice.innerText = `R$ ${preco},00`
+  cardDescription.innerText = descricao
+  cardButton.innerText = "Investir"
+
+  card.classList.add("card")
+
+  card.append(cartTitle, cardLine, cardPrice,cardDescription, cardButton)
+
+  return card
+}
+
+const listarCards = (array, selector) => {
+
+  for (let i = 0; i < array.length; i++) {
+    let cripto = array[i]
+    let card = criarCard(cripto)
+    selector.appendChild(card)
+  }
+}
+
+
+listarCards(cryptoMoedas, container)
+
 // Responsive carousel
 // =============================================================================
 
@@ -30,3 +92,6 @@ new Glider($responsiveCarousel, {
     },
   ],
 });
+
+//cards
+
